68c61ef8a5b534fcaa6312860c24edfce5c50940
Melo <Melo@mbp-de-melo.home>
2018-08-16 13:10:43 +0200

    fff

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,8 +14,9 @@ public class Greeter {
 
   /**
 * @param someone the name of person
-* @return ggreeting string 
+* @return ggreeting string
   */
+  
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
